46a99c419b1dae552ae39fdcb5afd57836acb281
@ontheroad_jp <dev@ontheroad.jp>
2016-03-16 22:55:21 +0900

    [bk]

--- src/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# -------------------------------------------------------
-# build nutsp/Lychee
-docker build -t nutsp/supervisord ./docker-supervisor
-docker build -t nutsp/lychee ./docker-lychee

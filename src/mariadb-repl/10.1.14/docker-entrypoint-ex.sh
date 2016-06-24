d825753e01ceb08afa8eb822c6659ffab1a73f5f
@ontheroad_jp <dev@ontheroad.jp>
2016-06-24 15:06:10 +0900

    [Modify] integrate /docker-entrypoint-initdb.d/master.sh into docker-entrypoint-ex.sh

--- src/mariadb-repl/10.1.14/docker-entrypoint-ex.sh
+++ src/mariadb-repl/10.1.14/docker-entrypoint-ex.sh
@@ -5,12 +5,12 @@ groupmod -g ${TOYBOX_GID} mysql
 
 #apt-get update -y && apt-get install -y vim
 
-out="/etc/mysql/conf.d/mariadb.cnf"
-sed -i -e "s:^#default-character-set *= *utf8:default-character-set = utf8:" ${out}
-sed -i -e "s:^#character-set-server *= *utf8:character-set-server = utf8:" ${out}
-sed -i -e "s:^#collation-server *= *utf8_general_ci:collation-server = utf8_general_ci:" ${out}
-sed -i -e "s:^#character_set_server *= *utf8:character_set_server = utf8:" ${out}
-sed -i -e "s:^#collation_server *= *utf8_general_ci:collation_server = utf8_general_ci:" ${out}
+maria_cnf="/etc/mysql/conf.d/mariadb.cnf"
+sed -i -e "s:^#default-character-set *= *utf8:default-character-set = utf8:" ${maria_cnf}
+sed -i -e "s:^#character-set-server *= *utf8:character-set-server = utf8:" ${maria_cnf}
+sed -i -e "s:^#collation-server *= *utf8_general_ci:collation-server = utf8_general_ci:" ${maria_cnf}
+sed -i -e "s:^#character_set_server *= *utf8:character_set_server = utf8:" ${maria_cnf}
+sed -i -e "s:^#collation_server *= *utf8_general_ci:collation_server = utf8_general_ci:" ${maria_cnf}
 
 chown -R mysql:mysql /var/log/mysql
 
@@ -36,9 +36,14 @@ innodb_support_xa=1
 ##[galera]
 ##innodb_flush_logs_at_trx_commit=1
 EOF
+    
+    master_ini="/docker-entrypoint-initdb.d/master_ini.sh"
+    cat << EOF > ${master_ini}
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT REPLICATION SLAVE ON *.* to repl@'%' identified by 'password';"
+EOF
 
 elif [ ${SERVER_TYPE} = 'slave' ]; then
-    cat << EOF > ${out}
+    cat << EOF > ${repl_cnf}
 [mysqld]
 server_id=2
 read_only
@@ -59,10 +64,10 @@ EOF
             --default-character-set=utf8 > ${dumpfile} | true
     done
     
-    out="/docker-entrypoint-initdb.d/change_master_to.sh"
+    slave_ini="/docker-entrypoint-initdb.d/slave_ini.sh"
     binarylog=$(head -n 100 master_db.sql | grep CHANGE | sed -e "s:^.*\(mariadb-bin.[0-9]*\).*$:\1:")
     binarylog_pos=$(head -n 100 master_db.sql | grep CHANGE | sed -e "s:^.*MASTER_LOG_POS=\([0-9]*\);.*$:\1:")
-    cat << EOF > ${out}
+    cat << EOF > ${slave_ini}
 mysql -u root -p${MYSQL_ROOT_PASSWORD} < ${dumpfile}
 mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CHANGE MASTER TO 
                              MASTER_HOST='master', 

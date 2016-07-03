5ed6c946171b0e574af718e9bc1a2e264d3721ad
@ontheroad_jp <dev@ontheroad.jp>
2016-07-03 22:25:44 +0900

    [Add] auto install plugins and jobs

--- src/jenkins/1.651.3/docker-entrypoint-ex.sh
+++ src/jenkins/1.651.3/docker-entrypoint-ex.sh
@@ -4,12 +4,13 @@ usermod -u ${TOYBOX_UID} jenkins
 groupmod -g ${TOYBOX_GID} jenkins
 
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
-#java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin git-client git github
+java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin git-client git github
+cat /usr/share/jenkins/config.xml | java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job docker-toybox
 
-out="/usr/share/jenkins/plugins.txt"
-echo "git-client" > ${out}
-echo "git" >> ${out}
-echo "github" >> ${out}
-/usr/local/bin/plugins.sh ${out}
+#out="/usr/share/jenkins/plugins.txt"
+#echo "git-client" > ${out}
+#echo "git" >> ${out}
+#echo "github" >> ${out}
+#/usr/local/bin/plugins.sh ${out}
 
 exec "$@"

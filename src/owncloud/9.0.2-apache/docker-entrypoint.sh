00cf269859775b356179d165c3ba409eb2b9ae77
@ontheroad_jp <dev@ontheroad.jp>
2016-07-10 12:21:27 +0900

    [Fix] change ${TIMEZONE} to $(date +%Z)

--- src/owncloud/9.0.2-apache/docker-entrypoint.sh
+++ src/owncloud/9.0.2-apache/docker-entrypoint.sh
@@ -10,7 +10,11 @@ docroot="/var/www/html"
 chown -R www-data:www-data ${docroot}
 #cd ${docroot}
 
-sleep 30 
+#while ! docker exec -it ${DB_CONTAINER_NAME} ps aux | grep "mysqld --user=m"; do
+#    echo "waiting for db setup..." && sleep 3
+#done
+
+sleep 30
 
 if [ -f "${docroot}/config/config.php" ]; then
     sed -i -e "s/'dbhost' => '.*\..*\..*\..*'/'dbhost' => '${MYSQL_PORT_3306_TCP_ADDR}'/g" ${docroot}/config/config.php
@@ -25,10 +29,10 @@ else
         --admin-pass "toybox"
 
     sudo -u www-data php /usr/src/owncloud/occ config:system:set trusted_domains \
-        0 --value ${VIRTUAL_HOST}
+        1 --value ${VIRTUAL_HOST}
 
     sudo -u www-data php /usr/src/owncloud/occ config:system:set logtimezone \
-        --value=${TIMEZONE}
+        --value=$(date +%Z)
 
     sudo -u www-data php /usr/src/owncloud/occ config:system:set memcache.local \
         --value="\OC\Memcache\APCu"
